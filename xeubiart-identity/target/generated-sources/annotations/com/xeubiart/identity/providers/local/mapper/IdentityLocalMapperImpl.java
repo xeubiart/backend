0020260c1bfb6cbb94eb161ce485cbb2cd917450
mateusLWD <alves.mateus1050@gmail.com>
2026-04-13 02:10:58 +0100

    .

--- xeubiart-identity/target/generated-sources/annotations/com/xeubiart/identity/providers/local/mapper/IdentityLocalMapperImpl.java
+++ xeubiart-identity/target/generated-sources/annotations/com/xeubiart/identity/providers/local/mapper/IdentityLocalMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-04-13T00:15:25+0100",
+    date = "2026-04-13T02:09:57+0100",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260224-0835, environment: Java 21.0.10 (Eclipse Adoptium)"
 )
 @Component
